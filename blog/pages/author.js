46875c21e3884ff41c9b22586d3c6ddc53131c67
Yukun Zhan <yukunzhan@Yukuns-Mac-mini.local>
2024-04-19 21:59:22 -0400

    my blog site

---
+++ blog/pages/author.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Author() {
+  return (
+    <>
+      <h1>About the Author</h1>
+
+      <p>
+        Yukun Zhan, a full stack developor.>_<:wq
+
+      </p>
+    </>
+  )
+}
+
+export default Author
